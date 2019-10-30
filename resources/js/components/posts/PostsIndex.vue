3657fd1767e10a5fd106c4789b76f293248bf84d
Remko Hummels <remko22393@gmail.com>
2019-10-30 09:55:17 -0700

    integrating with back-end api and implementing loading-overlay

--- resources/js/components/posts/PostsIndex.vue
+++ resources/js/components/posts/PostsIndex.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="post-trend-body">
+        <loading :active.sync="isLoading"
+                 :is-full-page="true"></loading>
         <div class="ui grid post-trend-header-container">
             <div class="left floated eight wide column post-trend-header">
                 Post trends
             </div>
             <div class="right floated eight wide column post-trend-filter mtop-5">
                 <div class="right-float ui dropdown" tabindex="0">
-                    <div class="text fs-16">This month</div>
+                    <div class="text fs-16">This week</div>
                     <i class="fal fa-angle-down icon"></i>
                     <div class="menu transition hidden dropdown-menu" tabindex="-1">
-                        <div class="item fs-16 pr-50 bb-1">This week</div>
-                        <div class="item fs-16 pr-50">This month</div>
+                        <div class="item fs-16 pr-50 bb-1" v-on:click="searchPost('1w')">This week</div>
+                        <div class="item fs-16 pr-50" v-on:click="searchPost('1m')">This month</div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="ui divider"></div>
-        <div class="ui grid">
+        <div class="ui grid" v-if="posts.hasOwnProperty('best_post')">
             <div class="eight wide column">
                 <div class="perform-title bold-font mtop-15">
                     Best performing
@@ -68,21 +70,34 @@
 </template>
 
 <script>
+    // Import component
+    import Loading from 'vue-loading-overlay';
+    // Import stylesheet
+    import 'vue-loading-overlay/dist/vue-loading.css';
+
     export default {
         data: function () {
             return {
-                posts: []
+                posts: [],
+                currentSearchTerm: '1w',
+                isLoading: false,
             }
+        },
+        components: {
+            Loading
         },
         mounted() {
             var app = this;
-            axios.get('/api/v1/posts')
+
+            app.isLoading = true;
+            axios.post('/api/v1/posts', {period: app.currentSearchTerm})
                 .then(function (resp) {
                     // Initializing dropdown menu
                     $('.ui.dropdown')
                         .dropdown();
 
                     app.posts = resp.data;
+                    app.isLoading = false;
                     console.log('posts', app.posts);
                 })
                 .catch(function (resp) {
@@ -91,7 +106,23 @@
                 });
         },
         methods: {
+            searchPost(searchTerm) {
+                var app = this;
 
+                if (app.currentSearchTerm !== searchTerm) {
+                    app.currentSearchTerm = searchTerm;
+                    app.isLoading = true;
+                    axios.post('/api/v1/posts', {period: searchTerm})
+                        .then(function (resp) {
+                            app.posts = resp.data;
+                            app.isLoading = false;
+                        })
+                        .catch(function (resp) {
+                            console.log(resp);
+                            alert("Could not load posts");
+                        });
+                }
+            },
         }
     }
 </script>
