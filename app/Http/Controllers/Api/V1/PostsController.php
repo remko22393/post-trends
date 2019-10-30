9489e21ca5885b6baf4b2e49d57da02e1f4f57fb
Remko Hummels <remko22393@gmail.com>
2019-10-30 05:29:54 +0100

    read all posts of instagram kicksta user

--- app/Http/Controllers/Api/V1/PostsController.php
+++ app/Http/Controllers/Api/V1/PostsController.php
@@ -3,18 +3,63 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PostsController extends Controller
 {
     /**
+     * @var $url -- JSON Data Source URL
+     */
+    public static $url = 'https://www.instagram.com/kicksta.co/?__a=1';
+
+
+    /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function index()
     {
-        //
+        $data = self::handle();
+
+        $username = $data['graphql']['user']['username'];
+        $fullname = $data['graphql']['user']['full_name'];
+
+        $postsCnt = $data['graphql']['user']['edge_owner_to_timeline_media']['count'];
+        $followersCnt = $data['graphql']['user']['edge_followed_by']['count'];
+        $followingCnt = $data['graphql']['user']['edge_follow']['count'];
+
+        Log::info('Username: ' . print_r($username, true));
+        Log::info('Full Name: ' . print_r($fullname, true));
+        Log::info('Summary: ' . print_r([
+                $postsCnt . ' posts',
+                $followersCnt . ' followers',
+                $followingCnt . ' following',
+            ], true));
+
+        $ownerEdges = $data['graphql']['user']['edge_owner_to_timeline_media']['edges'];
+        $posts = [];
+
+        foreach($ownerEdges as $edge) {
+            $aPost = [];
+
+            $aPost['thumbnail'] = $edge['node']['thumbnail_src'];
+            $aPost['likes_cnt'] = $edge['node']['edge_liked_by']['count'];
+            $aPost['comments_cnt'] = $edge['node']['edge_media_to_comment']['count'];
+            $aPost['taken_at'] = Carbon::createFromTimeStamp($edge['node']['taken_at_timestamp'])->toFormattedDateString();
+            $aPost['taken_at_timestamp'] = $edge['node']['taken_at_timestamp'];
+
+            $posts[] = $aPost;
+        }
+
+        Log::info('Posts Details: ' . print_r($posts, true));
+
+        return response()->json([], 200);
     }
 
     /**
@@ -81,5 +126,36 @@ class PostsController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+    /**
+     * Handle the api request payloads.
+     *
+     * @return mixed|null
+     * @return mixed|null
+     */
+    public static function handle()
+    {
+        try {
+            $client = new Client();
+
+            $response = $client->get(self::$url);
+
+            $output = $response->getBody()->getContents();
+
+            $result = json_decode($output, true, 512, JSON_OBJECT_AS_ARRAY);
+
+//            Log::info(print_r($result, true));
+
+            return $result;
+
+        } catch (ClientException $ex) {
+
+            Log::error(print_r($ex->getMessage(), true));
+
+            return null;
+
+        }
     }
 }
